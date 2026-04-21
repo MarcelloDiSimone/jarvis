31e626118d1beeb50ebc37854ccb0a411d46f7c1
Marcello di Simone <1266044+MarcelloDiSimone@users.noreply.github.com>
2026-04-22 00:25:31 +0200

    Add github pages

--- apps/jarvis-client/src/app/app.config.ts
+++ apps/jarvis-client/src/app/app.config.ts
@@ -3,20 +3,15 @@ import {
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { appRoutes } from './app.routes';
-import {
-  provideClientHydration,
-  withEventReplay,
-} from '@angular/platform-browser';
 import { provideAiCommandApi } from '@jarvis/speech-api-common';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideClientHydration(withEventReplay()),
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(appRoutes),
+    provideRouter(appRoutes, withHashLocation()),
     provideAiCommandApi(),
   ],
 };
