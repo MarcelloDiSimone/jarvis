c84658663637153a88187e876fb38ca5242b7a66
Marcello di Simone <1266044+MarcelloDiSimone@users.noreply.github.com>
2026-04-25 12:06:55 +0200

    Add PWA support

--- apps/jarvis-client/src/app/app.config.ts
+++ apps/jarvis-client/src/app/app.config.ts
@@ -1,11 +1,13 @@
 import {
   ApplicationConfig,
+  isDevMode,
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
 } from '@angular/core';
 import { provideRouter, withHashLocation } from '@angular/router';
 import { appRoutes } from './app.routes';
 import { provideAiCommandApi } from '@jarvis/speech-api-common';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,5 +15,9 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes, withHashLocation()),
     provideAiCommandApi(),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
 };
